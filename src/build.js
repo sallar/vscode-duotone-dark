21f43a60d295c7b225608f15b18ba2d51c097187
Sallar Kaboli <sallar.kaboli@gmail.com>
2017-02-14 15:21:16 +0200

    Improve build script

--- src/build.js
+++ src/build.js
@@ -1,26 +1,21 @@
 'use strict';
-const fs = require('fs-extra');
+
+const fs = require('fs');
 const path = require('path');
 const themes = require('./themes');
 
-let xmlTemplate = fs.readFileSync(path.join(__dirname, './theme.xml'));
-
-if (!xmlTemplate) {
-    throw new Error('Template does not exist');
-}
-
-xmlTemplate = xmlTemplate.toString('utf8');
+const xmlTemplate = fs.readFileSync(path.join(__dirname, './theme.xml')).toString('utf8');
 const themeList = Object.keys(themes);
 
 themeList.forEach(themeName => {
     const vars = themes[themeName];
-    let template = xmlTemplate.slice(0);
+    let template = xmlTemplate;
 
     for (let prop in vars) {
         template = template.replace(new RegExp(`__${prop}__`, 'g'), vars[prop]);
     }
     
-    fs.outputFileSync(path.join(__dirname, '../themes/', `${themeName}.tmTheme`), template);
+    fs.writeFileSync(path.join(__dirname, '../themes/', `${themeName}.tmTheme`), template);
 });
 
 console.log('Done writing themes');
